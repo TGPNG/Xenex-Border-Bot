e733f5d1836540adbad00a68106eb60da558838e
HoaX7 <krhoax7@gmail.com>
2021-02-03 23:31:18 +0530

    canvas

---
+++ canvas.js
@@ -0,0 +1,34 @@
+module.exports = {
+    async createSingleCanvas() {
+      try {
+        const canvas = createCanvas(750, 1000);
+        const ctx = canvas.getContext("2d");
+        const image = new Image();
+        image.src = `./chara1.png`; // chara img path here
+        const border = new Image();
+        // border.src = './temp.png';
+        border.src = `./border.png`; // border image path here
+        const borderCanvas = createCanvas(750, 1000);
+        const borderCtx = borderCanvas.getContext("2d");
+        borderCtx.drawImage(
+          border,
+          0,
+          0,
+          borderCanvas.width,
+          borderCanvas.height
+        );
+        borderCtx.globalCompositeOperation = "source-in";
+        borderCtx.fillStyle = "red"; // color here
+        borderCtx.fillRect(0, 0, borderCanvas.width, borderCanvas.height);
+        if (image.complete) {
+          ctx.drawImage(image, 30, 20, canvas.width, canvas.height);
+          ctx.drawImage(borderCanvas, 30, 20, canvas.width - 30, canvas.height);
+          return canvas;
+        }
+        return;
+      } catch (err) {
+        console.log(err);
+        return;
+      }
+    },
+}
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
 const Discord = require('discord.js');
+const { createSingleCanvas } = require('./canvas');
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -15,7 +16,12 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
-    msg.reply('pong!');
+    const embed = new Discord.MessageEmbed();
+    const canvas = createSingleCanvas();
+    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), "img.jpg");
+    embed.attachFiles(attachment)
+      .setImage("attachment://img.jpg");
+    msg.channel.send(embed);
   }
 });
 
